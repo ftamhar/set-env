2001a9c19ab02ec1063f101ce6b6c8284cd6944a
Rahmat Fathoni <noti.rahmat@gmail.com>
2022-02-06 08:28:21 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ftamhar/set-env
+
+go 1.17
---
+++ setEnv.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	var rootPath string
+
+	flag.StringVar(&rootPath, "f", ".env", "file env")
+	flag.Parse()
+
+	file, err := os.ReadFile(rootPath)
+	if err != nil {
+		log.Fatalf("\nfile %s not found\nplease set file with -f\nfor help: setEnv -h", rootPath)
+	}
+
+	arrEnv := strings.Split(string(file), "\n")
+
+	for _, v := range arrEnv {
+		idx := strings.Index(v, "=")
+
+		if idx < 1 {
+			continue
+		}
+
+		idxComment := strings.Index(v, "#")
+
+		// no comment on end env value
+		if idxComment == -1 {
+			setEnv(v[:idx], strings.Trim(v[idx+1:], " "))
+		} else if idxComment > idx { // have comment on end env value
+			setEnv(v[:idx], strings.Trim(v[idx+1:idxComment], " "))
+		}
+	}
+	syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, syscall.Environ())
+}
+
+func setEnv(key, val string) {
+	if os.Getenv(key) == "" {
+		os.Setenv(key, val)
+	}
+}
