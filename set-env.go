ad73593f8351a9a6a4cc88aaa859bb172a582a17
Rahmat Fathoni <mr.rahmat.fathoni@gmail.com>
2023-06-30 15:36:19 +0800

    feat: add option verbose (-v)

--- set-env.go
+++ set-env.go
@@ -9,24 +9,25 @@ import (
 	"syscall"
 )
 
-var boolReplace bool
+var (
+	boolReplace bool = true
+	verbose     bool = false
+)
 
 func main() {
-	boolReplace = true
 	var rootPath string
-
 	var replace string
+
 	flag.StringVar(&rootPath, "f", ".env", "file env")
 	flag.StringVar(&replace, "r", "true", "replace environment variables")
+	flag.BoolVar(&verbose, "v", false, "verbose")
 	flag.Parse()
 
 	if replace != "true" && replace != "false" {
 		log.Fatalf("option -r must be %q or %q", "true", "false")
 	}
 
-	if replace == "false" {
-		boolReplace = false
-	}
+	boolReplace = replace == "true"
 
 	file, err := os.ReadFile(rootPath)
 	if err != nil {
@@ -50,11 +51,13 @@ func main() {
 func setEnv(key, val string) {
 	before := os.Getenv(key)
 	if val[len(val)-1] == '"' && val[0] == '"' {
-		val = strings.Trim(val, "\"")
+		val = strings.Trim(val, `"`)
 	}
 	if before == "" || boolReplace {
 		if before != val {
-			fmt.Printf("%s : Before = %q, After = %q\n", key, before, val)
+			if verbose {
+				fmt.Printf("%s : Before = %q, After = %q\n", key, before, val)
+			}
 			os.Setenv(key, val)
 		}
 	}
